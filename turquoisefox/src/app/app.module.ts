cf184306f6cfd806e3d33e88a8e1cb711029058c
TurquoiseFox <codeblondmusic@gmail.com>
2019-02-04 13:24:30 +0100

    Add navigation and routing

--- turquoisefox/src/app/app.module.ts
+++ turquoisefox/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { TechSpecsComponent } from './tech-specs/tech-specs.component';
+import { ContactComponent } from './contact/contact.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    TechSpecsComponent,
+    ContactComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
